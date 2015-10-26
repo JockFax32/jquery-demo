2c03c5944d98c352d871b7dfda93d7a679bfa258
JockFax32 <foxtjack@gmail.com>
2015-10-26 10:14:24 -0700

    Commit

--- js/main.js
+++ js/main.js
@@ -2,3 +2,47 @@
 
 /* Use clever selections and event handlers to assign the described interactivity */
 
+var selector = null;
+var content = null;
+
+var getInput = function(){
+     selector = $('#selector').val();
+     content = $('#newContent').val();
+};
+
+$('button').click(getInput);
+
+$('#change-content').on('click',function(){
+   $(selector).text(content);
+})
+
+$('#add-at-end').on('click',function(){
+    $(selector).append(content);
+
+})
+
+$('#add-at-start').on('click',function(){
+
+
+})
+
+$('#insert-before').on('click',function(){
+    $(selector).before(content)
+
+})
+
+$('#move-after').on('click',function(){
+    $(selector).after(content);
+
+})
+
+$('#surrond-class').on('click',function(){
+
+})
+$('#hide-text').on('click',function(){
+
+})
+$('#remove-world').on('click',function(){
+
+})
+
